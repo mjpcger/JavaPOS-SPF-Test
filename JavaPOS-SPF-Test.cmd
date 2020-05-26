84c81daa544882358d67872fb5de1d81d965e6d1
Martin <37701263+mjpcger@users.noreply.github.com>
2020-05-26 19:55:11 +0200

    Binaries for Version 1
    Binaries for version 1 added.
    Installation instructions added.

---
+++ JavaPOS-SPF-Test.cmd
@@ -0,0 +1,74 @@
+@echo off
+set JAVA_HOME=C:\Program Files\Java\jre1.8.0_51
+set JFX_HOME=%JAVA_HOME%
+
+rem *********************************************************
+rem * Check if environment variable JAVA_HOME has been set. *
+rem *********************************************************
+
+if not "%JAVA_HOME%"=="" goto HomePresent
+	echo JAVA_HOME (path to current Java installation) not set.
+	goto :prompt
+:HomePresent
+if exist "%JAVA_HOME%\bin\java.exe" goto JavaPresent
+	echo No java.exe found in JAVA_HOME (%JAVA_HOME%).
+	goto :prompt
+:JavaPresent
+
+
+rem ***************************************************************
+rem * Check if environment variable JFX_HOME has been set. This   *
+rem * will not be done automatically during JavaFX installation!! *
+rem ***************************************************************
+
+if not "%JFX_HOME%"=="" goto FXPresent
+	echo JFX_HOME (path to current JavaFX installation) not set.
+	goto :prompt
+:FXPresent
+
+set VM_Flags=
+set classpath=
+
+if "%JAVA_HOME%"=="%JFX_HOME%" goto :JavaWithFX
+
+set classpath=%JFX_HOME%\lib\javafx.base.jar
+set classpath=%classpath%;%JFX_HOME%\lib\javafx.controls.jar
+set classpath=%classpath%;%JFX_HOME%\lib\javafx.fxml.jar
+set classpath=%classpath%;%JFX_HOME%\lib\javafx.graphics.jar
+set classpath=%classpath%;%JFX_HOME%\lib\javafx.media.jar
+set classpath=%classpath%;%JFX_HOME%\lib\javafx.swing.jar
+set classpath=%classpath%;%JFX_HOME%\lib\javafx.web.jar
+set classpath=%classpath%;%JFX_HOME%\lib\javafx-swt.jar
+
+set VM_Flags=--module-path %JFX_HOME%\lib --add-modules=javafx.controls,javafx.fxml
+
+:JavaWithFX
+
+rem ****************************************************************
+rem * Add jpos114, xalan-j and all other necessary jar files from  *
+rem * sub-folder jar                                               *
+rem ****************************************************************
+
+for %%k in (jar\*.jar) do call :addtoclasspath %%~dpfk
+
+rem Now call the Java command
+echo on
+
+"%JAVA_HOME%\bin\java.exe" %VM_Flags% -cp %classpath% SPF_Test.Main
+
+if %ERRORLEVEL%==0 goto :EOF
+
+:prompt
+set /P x=Press Enter to exit.
+goto :EOF
+
+### Subroutine addtoclasspath
+# Adds given jar file to environment variable classpath
+# and sets environment variable x to 1.
+# Parameter 1: Jar file
+#########################
+
+:addtoclasspath
+if not "%classpath%"=="" set classpath=%classpath%;
+set classpath=%classpath%%1
+goto :EOF
